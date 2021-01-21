414bf155f5c29081bd94a869ca8902517acdd546
Valerio Mariani <valmar@slac.stanford.edu>
2021-01-20 22:39:30 -0800

    Ran conda smithy skeleton

---
+++ recipe/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -x -e
+
+declare -a CMAKE_PLATFORM_FLAGS
+CMAKE_PLATFORM_FLAGS+=(-DCMAKE_FIND_ROOT_PATH="${PREFIX};${BUILD_PREFIX}/${HOST}/sysroot")
+CMAKE_PLATFORM_FLAGS+=(-DCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES:PATH="${BUILD_PREFIX}/${HOST}/sysroot/usr/include")
+
+if [[ ${DEBUG_C} == yes ]]; then
+  CMAKE_BUILD_TYPE=Debug
+else
+  CMAKE_BUILD_TYPE=Release
+fi
+
+
+export LDFLAGS="-lrt"
+export PKG_CONFIG_PATH=${CONDA_PREFIX}/share/pkgconfig:${CONDA_PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH
+export CXXFLAGS="-I ${CONDA_PREFIX}/include"
+
+env |sort
+
+mkdir build
+cd build
+${BUILD_PREFIX}/bin/cmake --version
+${BUILD_PREFIX}/bin/cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_PREFIX_PATH="${PREFIX}" \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+  -DBUILD_SHARED_LIBS=ON \
+  -DSTDCOMPAT_CXX_VERSION=11 \
+  -DBUILD_TESTING=OFF \
+  "${CMAKE_PLATFORM_FLAGS[@]}" \
+  ${SRC_DIR}
+
+${BUILD_PREFIX}/bin/make -j${CPU_COUNT}
+${BUILD_PREFIX}/bin/make install PREFIX=${PREFIX}
